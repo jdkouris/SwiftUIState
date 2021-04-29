7e12764749abaca683db7d7696a49bd8705e512b
John Kouris <jdkouris@gmail.com>
2021-04-29 09:16:14 -0700

    Made independent counters and a master counter

--- SwiftUIState/ContentView.swift
+++ SwiftUIState/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
+    @State private var totalTapCount = 0
+    @State private var redCount = 0
+    @State private var blueCount = 0
+    @State private var greenCount = 0
     
     var body: some View {
         VStack {
-            CounterButton(counter: $tapCount, color: .green)
-            CounterButton(counter: $tapCount, color: .blue)
-            CounterButton(counter: $tapCount, color: .red)
+            Text("\(totalTapCount)")
+                .font(.system(size: 150, weight: .heavy, design: .rounded))
+            
+            HStack {
+                CounterButton(counter: $greenCount, totalTapCount: $totalTapCount, color: .green)
+                CounterButton(counter: $blueCount, totalTapCount: $totalTapCount, color: .blue)
+                CounterButton(counter: $redCount, totalTapCount: $totalTapCount, color: .red)
+            }
         }
+    }
+}
+
+struct CounterButton: View {
+    @Binding var counter: Int
+    @Binding var totalTapCount: Int
+    
+    var color: Color
+    
+    var body: some View {
+        Button(action: {
+            counter += 1
+            totalTapCount += 1
+        }, label: {
+            Circle()
+                .frame(width: 100, height: 200)
+                .foregroundColor(color)
+                .overlay(
+                    Text("\(counter)")
+                        .font(.system(size: 50, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                )
+        })
     }
 }
 
@@ -23,28 +54,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-
-struct CounterButton: View {
-    @Binding var counter: Int
-    
-    var color: Color
-    
-    var body: some View {
-        Button(action: {
-            counter += 1
-        }, label: {
-            Circle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(color)
-                .overlay(
-                    Text("\(counter)")
-                        .font(.system(size: 100, weight: .bold, design: .rounded))
-                        .foregroundColor(.white)
-                )
-        })
-    }
-    
-    
 }
