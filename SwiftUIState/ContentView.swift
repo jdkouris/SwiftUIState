1127eedffa8968c0c4201acc4a52e7992d1e1e45
John Kouris <jdkouris@gmail.com>
2021-04-29 09:00:16 -0700

    Created counter button

--- SwiftUIState/ContentView.swift
+++ SwiftUIState/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isPlaying = false
+    @State private var tapCount = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: {
+                isPlaying.toggle()
+                tapCount += 1
+            }, label: {
+                ZStack {
+                    
+                    Circle()
+                        .frame(width: 200, height: 200, alignment: .center)
+                        .foregroundColor(isPlaying ? .red : .green)
+                    Text("\(tapCount)")
+                        .font(.system(size: 100))
+                        .foregroundColor(.white)
+                }
+            }).padding()
+            
+        }
     }
 }
 
