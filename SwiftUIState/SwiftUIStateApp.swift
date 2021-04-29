3db3269bd1ea3f5d76ad097dbc3270ec059d06ce
John Kouris <jdkouris@gmail.com>
2021-04-29 08:47:59 -0700

    Initial Commit

---
+++ SwiftUIState/SwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateApp.swift
+//  SwiftUIState
+//
+//  Created by John Kouris on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
