dcadf87584b5a26297351c4ba06712a4f89236dd
John Kouris <jdkouris@gmail.com>
2021-04-29 09:09:24 -0700

    Created reusable CounterButton struct and added 3 to app

--- SwiftUIState/ContentView.swift
+++ SwiftUIState/ContentView.swift
@@ -8,23 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var isPlaying = false
     @State private var tapCount = 0
     
     var body: some View {
-        Button(action: {
-            
-        }, label: {
-            Circle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(.green)
-                .overlay(
-                    Text("\(tapCount)")
-                        .font(.system(size: 100, weight: .bold, design: .rounded))
-                        .foregroundColor(.white)
-                )
-        })
+        VStack {
+            CounterButton(counter: $tapCount, color: .green)
+            CounterButton(counter: $tapCount, color: .blue)
+            CounterButton(counter: $tapCount, color: .red)
+        }
     }
 }
 
@@ -32,4 +23,28 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct CounterButton: View {
+    @Binding var counter: Int
+    
+    var color: Color
+    
+    var body: some View {
+        Button(action: {
+            counter += 1
+        }, label: {
+            Circle()
+                .frame(width: 200, height: 200)
+                .foregroundColor(color)
+                .overlay(
+                    Text("\(counter)")
+                        .font(.system(size: 100, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                )
+        })
+    }
+    
+    
 }
